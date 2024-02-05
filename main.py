b895c343ac4dd4a4caa403cfb08ba21b10cc50f7
WilliamSlamat136 <138879529+WilliamSlamat136@users.noreply.github.com>
2024-02-05 18:44:18 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-from settings import settings
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
 import discord
-# import * - adalah cara cepat untuk mengimpor semua file di perpustakaan
-from bot_logic import *
+from discord.ext import commands
+import random
 
-# Variabel intents menyimpan hak istimewa bot
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
 intents = discord.Intents.default()
-# Mengaktifkan hak istimewa message-reading
+intents.members = True
 intents.message_content = True
-# Membuat bot di variabel klien dan memindahkan hak istimewa
-client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 
-# Setelah bot siap, ia akan mencetak namanya!
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
 
-# Saat bot menerima pesan, bot akan mengirimkan pesan di saluran yang sama!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Saya! Saya bot!')
-    elif message.content.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send("Tidak dapat memproses perintah ini, maaf")
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
 
-client.run(settings["TOKEN"])
+@bot.command()
+async def mem(ctx):
+    with open('images/mem2.jpg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+    
+bot.run('MTE5ODk0NzU1NjgzOTkyMzczMw.GURn4q.n67hwpNBgPuIRbCi0Yo6KuBuJA0AAl3Q7kRCHg')
