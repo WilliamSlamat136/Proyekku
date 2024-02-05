a5f60cc531a3afe3f97f0e02cb028515af6cade2
WilliamSlamat136 <138879529+WilliamSlamat136@users.noreply.github.com>
2024-02-05 18:42:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,79 +1,36 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
-
+from settings import settings
 import discord
-from discord.ext import commands
-import random
+# import * - adalah cara cepat untuk mengimpor semua file di perpustakaan
+from bot_logic import *
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-
-There are a number of utility commands being showcased here.'''
-
+# Variabel intents menyimpan hak istimewa bot
 intents = discord.Intents.default()
-intents.members = True
+# Mengaktifkan hak istimewa message-reading
 intents.message_content = True
+# Membuat bot di variabel klien dan memindahkan hak istimewa
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
-
-@bot.event
+# Setelah bot siap, ia akan mencetak namanya!
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    print(f'We have logged in as {client.user}')
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
+# Saat bot menerima pesan, bot akan mengirimkan pesan di saluran yang sama!
+@client.event
+async def on_message(message):
+    if message.author == client.user:
         return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Saya! Saya bot!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send("Tidak dapat memproses perintah ini, maaf")
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
-
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
-
-
-bot.run('token')
+client.run(settings["TOKEN"])
---
+++ settings.py
@@ -0,0 +1,4 @@
+settings = {
+    "prefix": ">",
+    "TOKEN": "MTE5ODk0NzU1NjgzOTkyMzczMw.GURn4q.n67hwpNBgPuIRbCi0Yo6KuBuJA0AAl3Q7kRCHg"
+}
